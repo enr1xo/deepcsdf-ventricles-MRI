f27b089b7382d6017ff524d0714090aa7bc381c0
Davide <davide.navarri@gmail.com>
2026-01-29 18:18:17 +0100

    updated

--- launch_training_bash/test_versions.sh
+++ launch_training_bash/test_versions.sh
@@ -6,8 +6,8 @@ PYTHON_SCRIPT=test.py
 TEST_DATASET="train/data_fnames_train-20patients.json"
 SLEEP_INTERVAL=300       # seconds
 SAFETY_MARGIN_MB=500   # safety
-MEM_REQUIRED_MB=2000    # with 89 scenes with 100000 points each all loaded in GPU,  2^14 points per scene, batches of 16 scenes, model with latent size 128, 512 x 7 layers, all lipschitz, --> memory usage tops out at 5108MiB /  24576MiB 
-MAX_PARALLEL=4 
+MEM_REQUIRED_MB=6000    # with 89 scenes with 100000 points each all loaded in GPU,  2^14 points per scene, batches of 16 scenes, model with latent size 128, 512 x 7 layers, all lipschitz, --> memory usage tops out at 5108MiB /  24576MiB 
+MAX_PARALLEL=3 
 LOG_DIR=experiments/logs-temp
 # --------------------
 
@@ -39,29 +39,32 @@ declare -a JOB_PIDS=()
 for dir in "$VERSION_DIR"/*/; do
     ver=${dir%/}
     ver="${ver##*/}"
-    
-    while true; do
-        clean_jobs
-        free=$(free_mem_mb)
-        running="${#JOB_PIDS[@]}"
 
-        echo "Free GPU memory: ${free}MB | Running jobs: ${running}"
+    if [ "$ver" != "version_34" ]; then
+            
+        while true; do
+            clean_jobs
+            free=$(free_mem_mb)
+            running="${#JOB_PIDS[@]}"
 
-        # Check if enough memory + below max jobs
-        if (( free > MEM_REQUIRED_MB + SAFETY_MARGIN_MB && running < MAX_PARALLEL )); then
-            echo "Launching $ver"
-            logfile="$LOG_DIR/$ver.log"
+            echo "Free GPU memory: ${free}MB | Running jobs: ${running}"
 
-            python "$PYTHON_SCRIPT" -e "training_sweeps/LipAndAct" -v "$ver" -od "$TEST_DATASET" -cm &> "$logfile" &
+            # Check if enough memory + below max jobs
+            if (( free > MEM_REQUIRED_MB + SAFETY_MARGIN_MB && running < MAX_PARALLEL )); then
+                echo "Launching $ver"
+                logfile="$LOG_DIR/$ver.log"
 
-            JOB_PIDS+=($!)
-            sleep 10
-            break
-        else
-            echo "Waiting for GPU memory..."
-            sleep "$SLEEP_INTERVAL"
-        fi
-    done
+                python "$PYTHON_SCRIPT" -e "training_sweeps/LipAndAct" -v "$ver" -od "$TEST_DATASET" -cm &> "$logfile" &
+
+                JOB_PIDS+=($!)
+                sleep 10
+                break
+            else
+                echo "Waiting for GPU memory..."
+                sleep "$SLEEP_INTERVAL"
+            fi
+        done
+    fi
 
 done
 
