af5be9213518bdc7b2bd1377feff9ca4d047a0a3
Davide <davide.navarri@gmail.com>
2026-02-03 10:07:09 +0100

    commented config file

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
 from pathlib import Path
 from loguru import logger
 
-# Paths
+# Paths: root folder
 PROJ_ROOT = Path(__file__).resolve().parents[0]
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
-PATIENT_MESHES_DIR = Path("/home/navarri/AtriaProject/DATASETS/AtrialGeometries")
+# PATIENT_MESHES_DIR = Path("/home/navarri/AtriaProject/DATASETS/AtrialGeometries")
 
-# PATIENT_MESHES_DIR = Path("/home/davidenava_linux/DATASETS/AtrialGeometries")
+# Directory where .vtu files of meshes are expected to be found
+PATIENT_MESHES_DIR = Path("/home/davidenava_linux/DATASETS/AtrialGeometries")
 
+# Just to help extract wanted surfaces by cell tags
 ATRIA_TAGS_METADATA = {
 
     "CUSTOM_LABELS": {
@@ -44,32 +46,41 @@ ATRIA_TAGS_METADATA = {
     "valve_names": ["MV", "TV"]
 }
 
-DATA_DIR = PROJ_ROOT / "data"
+# PATIENTS_COORDS_AND_SDFS_DIR = Path("/home/navarri/AtriaProject/DATASETS/AtriaPointsAndSDF")
 
-PATIENTS_COORDS_AND_SDFS_DIR = Path("/home/navarri/AtriaProject/DATASETS/AtriaPointsAndSDF")
+# PATIENTS_NPY_DATA_DIR =  PATIENTS_COORDS_AND_SDFS_DIR / "single_patients_100000pts_npy"
 
-PATIENTS_NPY_DATA_DIR =  PATIENTS_COORDS_AND_SDFS_DIR / "single_patients_100000pts_npy"
+PATIENTS_COORDS_AND_SDFS_DIR = Path("/home/davidenava_linux/DATASETS/AtriaPointsAndSDFs")
 
-# PATIENTS_COORDS_AND_SDFS_DIR = Path("/home/davidenava_linux/DATASETS/AtriaPointsAndSDFs")
-
-# PATIENTS_NPY_DATA_DIR =  PATIENTS_COORDS_AND_SDFS_DIR / "single_patients_50000pts_npy"
+# Folder where to find single .npy files with coords and sdf for each anatomy
+PATIENTS_NPY_DATA_DIR =  PATIENTS_COORDS_AND_SDFS_DIR / "single_patients_50000pts_npy"
 
 SPECS_FILES_DIR = PROJ_ROOT / "specs_files"
 
+# Directory under which all the experiments and training results are created (from train.py)
 EXPERIMENTS_DIR = PROJ_ROOT / "experiments"
 
-TRAIN_DATA_DIR = DATA_DIR / "train"
+# Directory where .json files for training or testing are stored (see README) 
+DATA_DIR = PROJ_ROOT / "data"
 
+# other subdirectories storing actual .json files
+TRAIN_DATA_DIR = DATA_DIR / "train"
 TEST_DATA_DIR = DATA_DIR / "test"
 
+# Where dataloader, decoder, and training module scripts are
 MODELS_DIR = PROJ_ROOT / "models"
 
+# Directory under which testing results are saved (from test.py)
 RESULTS_DIR = PROJ_ROOT / "results"
 
+# Images of reconstructed meshes
 IMAGES_DIR = RESULTS_DIR / "images"
 
+# Actual .vtu or .vtp files of reconstructed meshes
 RECONSTRUCTED_MESHES_DIR = RESULTS_DIR / "reconstructed"
 
+# Fitted latents
 LATENTS_DIR = RESULTS_DIR / "fitted_latents"
 
+# Metrics
 METRICS_DIR = RESULTS_DIR / "metrics"
