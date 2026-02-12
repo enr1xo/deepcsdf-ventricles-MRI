5deefba4f3a868300ee273126251b942515c12bd
Davide Navarri <davide.navarri@medunigraz.at>
2026-02-12 18:23:23 +0100

    updated

--- multiple_runs_launcher.py
+++ multiple_runs_launcher.py
@@ -1,7 +1,9 @@
 import json
 from pprint import pprint
 from multiprocessing import Process, set_start_method
-import torch
+import sys
+import os
+from pathlib import Path
 from train import train, check_override_specs_validity, deep_update
 
 # # ---------------------- Setup GPU memory ----------------------
@@ -10,6 +12,15 @@ from train import train, check_override_specs_validity, deep_update
 # torch.cuda.set_per_process_memory_growth(True)
 
 SPECS_BASE_FILE = "specs_files/specs_deepsdfatria.json"
+
+# If running inside Slurm, use $SLURM_JOB_ID, else fallback to PID or timestamp
+slurm_job_id = os.environ.get("SLURM_JOB_ID", None)
+if slurm_job_id is not None:
+    log_dir = Path(f"/home/isilon/users/o_navarri/experiments/logs-train-temp/{slurm_job_id}")
+else:
+    import time
+    log_dir = Path(f"/home/isilon/users/o_navarri/experiments/logs-train-temp/{int(time.time())}")
+
 
 # ---------------------- Hyperparameter Sweep ----------------------
 EXPERIMENT_NAME = "LipAlphaAndCodeReg"
@@ -22,24 +33,30 @@ with open(SPECS_BASE_FILE, "r") as f:
 
 # ---------------------- Function to launch a single training ----------------------
 def run_single_training(alpha, sigma):
-    override_specs = {
-        "Network_specs": {
-            "lipschitz_layers": [-1],
-            "use_lipschitz_normalized_layers": True
-        },
-        "lipschitz_alpha": alpha,
-        "code_reg_lambda": sigma
-    }
 
-    # Validate overrides
-    check_override_specs_validity(override_specs, base_specs)
-    specs = deep_update(base_specs.copy(), override_specs)
+    logfile = log_dir / f"Lipalpha{alpha}_CodeReg{sigma}.log"
+    with open(logfile, "w") as f:
+        sys.stdout = f
+        sys.stderr = f
 
-    print(f"Starting training: alpha={alpha}, sigma={sigma}")
-    pprint(specs)
+        override_specs = {
+            "Network_specs": {
+                "lipschitz_layers": [-1],
+                "use_lipschitz_normalized_layers": True
+            },
+            "lipschitz_alpha": alpha,
+            "code_reg_lambda": sigma
+        }
 
-    # call your existing train function
-    train(specs)
+        # Validate overrides
+        check_override_specs_validity(override_specs, base_specs)
+        specs = deep_update(base_specs.copy(), override_specs)
+
+        print(f"Starting training: alpha={alpha}, sigma={sigma}")
+        pprint(specs)
+
+        # call your existing train function
+        train(specs)
 
 # ---------------------- Launch all experiments concurrently ----------------------
 if __name__ == "__main__":
@@ -52,6 +69,7 @@ if __name__ == "__main__":
             p = Process(target=run_single_training, args=(alpha, sigma))
             p.start()
             processes.append(p)
+            time.sleep(15)
 
     # wait for all to finish
     for p in processes:
