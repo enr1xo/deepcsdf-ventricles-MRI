f2773366e8c00f34a6be607982f78479c638367f
enrico <enrico.rizzardi1@gmail.com>
2026-09-06 18:53:20 +0200

    differnt path

--- Z_enricos_stuff/visualize_npy.py
+++ Z_enricos_stuff/visualize_npy.py
@@ -10,7 +10,9 @@ import pyvista as pv
 #NPY_PATH = Path("/home/rizzardi/Schreibtisch/MRI_model/generated_npy_three_axis_LA_volume_2mm")
 #CARDIAC_SURFS_PATH = Path("/home/rizzardi/Schreibtisch/AF001_aligned_processed")
 
-NPY_PATH = Path(r"/home/rizzardi/Schreibtisch/old_model_on_MRI/expanded_dataset_40kpts_npy_MRIcomparison/S_0.025-L_0.75-R_0.5_50k_samples")
+
+#NPY_PATH = Path(r"/home/rizzardi/Schreibtisch/old_model_on_MRI/expanded_dataset_40kpts_npy_MRIcomparison/S_0.025-L_0.75-R_0.5_50k_samples")
+NPY_PATH = Path(r"/home/rizzardi/Schreibtisch/MRI_model/generated_npy_three_axis_grid_contourSDF_LAXband2mm_SAXbandNone_curvature_sampling/")
 CARDIAC_SURFS_PATH = Path(r"/home/rizzardi/Schreibtisch/AF001_aligned_processed")
 #NPY_PATH = Path(r"C:\Users\e.rizzardi\OneDrive\Desktop")
 
